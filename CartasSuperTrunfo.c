8c7d75f7f1ecbb7832131a200ecd97a3d50a3655
andrecoura23 <coura.andre21@gmail.com>
2025-02-21 11:24:47 +0000

    Criei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,9 @@
 #include <stdio.h>
 
-int main() {
-   int populacao;
-   int Numero, Pontos, turisticos;
-   float Área;
-   float PIB;
-   char Estado;
-   char Código [50];
-   char Nome, da, cidade [50];
-    
-   printf("Carta: \n");
-
-   printf("Qual seu estado: \n");
-   scanf("%s", &Estado);
-
-   print("Insira o Codigo: \n");
-   scanf("%s", &Código);
-
-   printf("Qual é a população: \n");
-   scanf ("%d", &populacao);
-
-   printf("Digite À Área: \n");
-   scanf("%f", &Área);
-
-   printf("Digite o PIB: \n");
-   scanf("%f", &PIB);
-
-   printf("Quantos Pontos turisticos tem:");
-   scanf("%d", &Numero); 
+int main(){
+ printf("Cartas Super Trunfo!\n");
 
 
-    return 0;
+return 0;
 
 }
